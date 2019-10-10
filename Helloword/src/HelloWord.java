236bd758381e2738ae1abdd67546031d12266b69
wcy <467672270@qq.com>
2019-10-10 14:37:40 +0800

    my testone commit

---
+++ Helloword/src/HelloWord.java
@@ -0,0 +1,10 @@
+/**
+ * @PackageName:PACKAGE_NAME Description
+ * @author:王朝阳
+ * @date:2019/10/10
+ */
+public class HelloWord {
+    public static void main(String[] args){
+        System.out.println("请输出:"+"HelloWord");
+    }
+}
