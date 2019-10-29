8895b522523c740c76c06a27371a735c52f51fda
wcy <467672270@qq.com>
2019-10-29 15:34:41 +0800

    who are you

---
+++ src/main/java/com/hello/web/Who.java
@@ -0,0 +1,12 @@
+package com.hello.web;
+
+/**
+ * @PackageName:com.hello.web Description
+ * @author:王朝阳
+ * @date:2019/10/29
+ */
+public class Who {
+    public String who(){
+        return "who are you";
+    }
+}
